878a894e51b634e5dc9e08a1fbd8ee2b4000537c
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2022-08-13 18:19:30 +0200

    created table

---
+++ GuestbookDB/dbo/Tables/Guestbook.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Guestbook]
+(
+	[postId] INT NOT NULL, 
+    [name] NCHAR(50) NOT NULL, 
+    [email] NCHAR(50) NOT NULL, 
+    [password] NCHAR(50) NOT NULL, 
+    [comment] NCHAR(255) NOT NULL, 
+    [datetime] DATETIME2 NOT NULL,
+    PRIMARY KEY (postId, email)
+)
