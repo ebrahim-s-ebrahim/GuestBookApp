abbf85ccff1c0751e41cbc65be0a0fbd5b8b6ca9
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2022-08-13 20:03:11 +0200

    Model created

---
+++ DataAccess/Models/GuestbookModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class GuestbookModel
+    {
+        public int PostId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Comment { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
